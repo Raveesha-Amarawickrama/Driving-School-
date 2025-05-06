01dc4549160da84f36c8c4eb3fdc9b0106434b84
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-05-06 23:34:21 +0530

    add footer.php

---
+++ includes/footer.php
@@ -0,0 +1,63 @@
+<!-- Footer -->
+<footer class="footer">
+        <div class="container">
+            <div class="footer-content">
+                <div class="footer-col">
+                   
+                    <div class="footer-logo">
+                        <a href="index.php">
+                            <h2><i class="fas fa-car"></i> Drivin</h2>
+                        </a>
+                    </div>
+                    <div class="footer-contact">
+    <p><i class="fas fa-map-marker-alt"></i> 123 Street, Dalugama, Kelaniya</p>
+    <p><i class="fas fa-phone-alt"></i> 
+        <a href="tel:+94762219168">+94 762219168</a>
+    </p>
+    <p><i class="far fa-envelope"></i> 
+        <a href="mailto:info@example.com">info@example.com</a>
+    </p>
+</div>
+
+                </div>
+                
+                <div class="footer-col">
+                  
+                    <ul class="footer-links">
+                        <li><a href="about.php"><i class="fas fa-angle-right"></i> About Us</a></li>
+                        <li><a href="contact.php"><i class="fas fa-angle-right"></i> Contact Us</a></li>
+                        <li><a href="courses.php"><i class="fas fa-angle-right"></i> Our Courses</a></li>
+                            </ul>
+                </div>
+                
+              
+                
+
+                    <div class="social-links">
+                        <a href="https://x.com/HashaniRav92602"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.facebook.com/profile.php?id=100089544730818"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.youtube.com/watch?v=CCCUTqJ3dBA&list=PLzo8Xp5O7px2raRp5ixZEsVfxyYlbM6K6"><i class="fab fa-youtube"></i></a>
+                        <a href="https://www.linkedin.com/in/raveesha-amarawickrama-1383592a4/"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+             <!-- Back to Top Button -->
+        <a href="#" class="back-to-top"><i class="fas fa-arrow-up"></i></a>
+ <hr>
+            <div class="footer-bottom">
+                <div class="copyright">
+                    <p>&copy; raveesha amarawickrama, All Right Reserved.</p>
+                </div>
+                <div class="credits">
+                    <p>Designed By <a href="#">Raveesha</a> </p>
+                </div>
+            </div>
+        </div>
+        
+          </footer>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
